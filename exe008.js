d1bd96eb51136032d611dbae67d4040ef7e4d2d8
Karina Rossler Fadel <karinafadel.7@gmail.com>
2021-12-01 08:08:55 -0300

    exercicios utilizando Java

---
+++ exe008.js
@@ -0,0 +1,9 @@
+var vel=60.5
+
+    console.log(`A velocidade do carro é ${vel} Km/h`)
+    if(vel>60){ //condição simples não precisa de else
+        console.log('Você Ultrapassou o limite de velocidade. Multado!')
+    }
+
+    console.log('Diriga sempre usando cinto de segurança')
+    
