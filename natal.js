d98de20742d1f8a57a1ba2abda9fcf3a048c180e
Karina Rossler Fadel <karinafadel.7@gmail.com>
2021-12-03 07:45:07 -0300

    ecercicios em java

---
+++ natal.js
@@ -0,0 +1,11 @@
+const hoje = new Date();
+const natal = new Date(hoje.getFullYear(), 11, 25);
+const um_dia = 1000*60*60*24; //milisegundos
+
+if (hoje.getMonth()==11 && hoje.getDate()>25)
+natal.setFullYear(natal.getFullYear()+1);
+
+const dias = Math.ceil(
+    (natal.getTime() - hoje.getTime()) / (um_dia)
+);
+console.log(`Faltam ${dias} dia(s) para o Natal!`);
