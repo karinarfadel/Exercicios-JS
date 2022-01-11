082b68d44f67ff67f39317d5b54553cf6ff1b85d
Karina Rossler Fadel <karinafadel.7@gmail.com>
2022-01-11 12:01:16 -0300

    exercício analisador de números

---
+++ funções/ex021.js
@@ -0,0 +1,11 @@
+function soma(n1=0,n2=0){
+    return n1+n2
+}
+console.log(soma(7))
+
+//se não colocar 0 eo returtado dará NaN=not a number
+/*function soma(n1=0,n2=0){
+    return n1+n2
+}
+console.log(soma(7))*/
+//para somar 2 numeros colocar (7,2)
